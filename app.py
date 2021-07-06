34e19c8c035a69183e6d163d9c32cfa8dbd6f367
Iwanttobeatuna <zadchin@gmail.com>
2021-07-06 16:38:09 +0800

    highlight fixed with minor bugs on repeating

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template,url_for,request
 from ability_skills_decoder import decoder
-import spacy
+from termcolor import colored
+from functools import reduce
+from itertools import chain
 
 app = Flask(__name__)
 TEMPLATES_AUTO_RELOAD = True
@@ -15,10 +17,22 @@ def predict():
         message = request.form['message']
         data = message
         ableist_language = decoder.find_ableist_language(data)
+
+        ## Finding number of ableist words
         length = len(ableist_language)
 
+        ls = [word.text for word in ableist_language]
 
-    return render_template('result.html',prediction = ableist_language, length=length)
+        newlist = [word for line in ls for word in line.split()]
+
+        ## highlighting words
+        ## def analyze():
+            ## print(reduce(lambda t, x: t.replace(*x), chain([data.lower()], "<mark>{}</mark>".format(word) for t in ls))))
+        
+        boldened = " ".join(["<mark>{}</mark>".format(word) if word in newlist else word for word in data.split()])
+
+
+    return render_template('result.html',prediction = ableist_language, length=length, ls=ls, newlist=newlist, data=data, result=boldened)
 
 if __name__ == '__main__':
 	app.run(debug=True)
