5edae53df25dbb9651cb31813b76974bf5b1f615
Iwanttobeatuna <zadchin@gmail.com>
2021-07-08 22:18:47 +0800

    highlight function

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from termcolor import colored
 from functools import reduce
 from itertools import chain
 
+## Things to do in back-end:
+## - Maintain the formatting
+## - Highlight the words
+
 app = Flask(__name__)
 TEMPLATES_AUTO_RELOAD = True
 
@@ -25,14 +29,24 @@ def predict():
 
         newlist = [word for line in ls for word in line.split()]
 
-        ## highlighting words
+        ## highlighting words python function
         ## def analyze():
-            ## print(reduce(lambda t, x: t.replace(*x), chain([data.lower()], "<mark>{}</mark>".format(word) for t in ls))))
+            ## print(reduce(lambda t, x: t.replace(*x), chain([data.lower()], ((t, colored(t,'white','on_red')) for t in ls))))
         
         boldened = " ".join(["<mark>{}</mark>".format(word) if word in newlist else word for word in data.split()])
 
+        
+        def highlight(paragraph):
+            for word in ls:
+                if word in paragraph:
+                    paragraph.replace(word, '<b>{}</b>'.format(word))
+                else:
+                    return paragraph
+        
+        s2=highlight(data)
 
-    return render_template('result.html',prediction = ableist_language, length=length, ls=ls, newlist=newlist, data=data, result=boldened)
+    
+    return render_template('result.html',prediction = ableist_language, length=length, ls=ls, newlist=newlist, data=data, result=boldened, s2=s2)
 
 if __name__ == '__main__':
 	app.run(debug=True)
