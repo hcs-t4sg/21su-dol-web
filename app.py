9e1d06c28fc4344bd748715b60eea889d24fc0bf
Jamie Lu <jamielu@Jamies-MacBook-Pro.local>
2021-07-19 15:16:37 -0700

    full output included

--- app.py
+++ app.py
@@ -21,6 +21,19 @@ def predict():
         message = request.form['message']
         data = message
         ableist_language = detector.find_ableist_language(data)
+        alternatives = {}
+        examples = {}
+        for i, ableist_term in enumerate(ableist_language):
+            term = str(ableist_term)
+            alternatives[term] = ableist_term.data.alternative_verbs
+            examples[term] = str(ableist_term.data.example)
+            # print(
+            #     f"Match #{i+1}\n"
+            #     f"PHRASE: {ableist_term} | LEMMA: {ableist_term.lemma} | "
+            #     f"POSITION: {ableist_term.start}:{ableist_term.end} | "
+            #     f"ALTERNATIVES: {ableist_term.data.alternative_verbs} | "
+            #     f"EXAMPLE: {ableist_term.data.example}\n"
+            # )
 
         ## Finding number of ableist words
         length = len(ableist_language)
@@ -30,7 +43,7 @@ def predict():
         for word in ls:
             data = data.replace(word, '<mark style="background: #00ced1!important">%s</mark>' % word)
     
-    return render_template('result.html',prediction = ableist_language, length=length, ls=ls, result=data)
+    return render_template('result.html',prediction = ableist_language, length=length, ls=ls, result=data, alternatives=alternatives, examples=examples)
 
 if __name__ == '__main__':
 	app.run(debug=True)
