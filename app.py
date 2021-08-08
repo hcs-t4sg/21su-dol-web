2938f6b72d11cd9364a0021f4e61986c78d41a27
Iwanttobeatuna <zadchin@gmail.com>
2021-08-08 11:58:24 +0800

    footer fixed, layout.html cleaned up

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask,render_template,url_for,request
-from itertools import chain
-import ableist_language_module.ableist_language_detector.detector as detector
+import ableist_language_detector.detector as detector
 from highlight import highlight, AbleistLanguage
 
 app = Flask(__name__)
